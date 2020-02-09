37c4c336d5fe2a46635a223059f38675b0e62147
jm-85-au <60832316+jm-85-au@users.noreply.github.com>
2020-02-09 14:14:58 +1100

    Update DatabaseCollection.java

--- src/com/app/db/constant/DatabaseCollection.java
+++ src/com/app/db/constant/DatabaseCollection.java
@@ -22,11 +22,11 @@ public class DatabaseCollection
 	/** --- PUBLIC VARIABLES                           --- **/
 	/** -------------------------------------------------- **/
     
-	public static final MongoCollection<Company> COMPANY =					DatabaseConnection.MONGO_DATABASE.getCollection("Company", Company.class);
+	public static final MongoCollection<Company> COMPANY =				DatabaseConnection.MONGO_DATABASE.getCollection("Company", Company.class);
 	public static final MongoCollection<EmailInvite> EMAIL_INVITE =			DatabaseConnection.MONGO_DATABASE.getCollection("EmailInvite", EmailInvite.class);
 	public static final MongoCollection<PasswordReset> PASSWORD_RESET =		DatabaseConnection.MONGO_DATABASE.getCollection("PasswordReset", PasswordReset.class);
-	public static final MongoCollection<Project> PROJECT =					DatabaseConnection.MONGO_DATABASE.getCollection("Project", Project.class);
+	public static final MongoCollection<Project> PROJECT =				DatabaseConnection.MONGO_DATABASE.getCollection("Project", Project.class);
 	public static final MongoCollection<ProjectTask> PROJECT_TASK =			DatabaseConnection.MONGO_DATABASE.getCollection("ProjectTask", ProjectTask.class);
-	public static final MongoCollection<User> USER =						DatabaseConnection.MONGO_DATABASE.getCollection("User", User.class);
+	public static final MongoCollection<User> USER =				DatabaseConnection.MONGO_DATABASE.getCollection("User", User.class);
 	public static final MongoCollection<UserPassword> USER_PASSWORD =		DatabaseConnection.MONGO_DATABASE.getCollection("UserPassword", UserPassword.class);
 }
